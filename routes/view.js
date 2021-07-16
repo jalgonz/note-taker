c7706b966ddb79b3b0c92b02510667732082f013
Joshua Gonzalez <jalgonz96@gmail.com>
2021-07-15 23:36:42 -0400

    final fix

--- routes/view.js
+++ routes/view.js
@@ -1,14 +1,15 @@
 const path = require("path");
-const router = require("express").Router();
 
-//Route for index page
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
 
-//Route for notes page
-router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
-  
-module.exports = router;
+function view (app) {
+    //route to notes page
+  app.get("/notes", (req, res) => 
+  res.sendFile(path.join(__dirname,"../public/notes.html")));
+
+  // route to index page
+  app.get("*", (req, res) => 
+  res.sendFile(path.join(__dirname,"../public/index.html")));
+};
+
+// Exporting file
+module.exports = view;
